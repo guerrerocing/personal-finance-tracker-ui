ce46669fc296a9a2d0bdddf934ce41a6d07d0f9d
guerrerocing <guerrerocing@outlook.com>
2023-09-11 19:37:02 -0600

    chore: Dev dependencies Configuration * Tailwind Added * PostCss * React Route * NextUI * Axios * Types * FontAwesome

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 import { nextui } from "@nextui-org/react";
 export default {
-  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
