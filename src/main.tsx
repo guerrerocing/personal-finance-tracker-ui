c267486379234348964f44f53761d927fd7dd8fa
guerrerocing <guerrerocing@outlook.com>
2023-09-11 19:43:01 -0600

    feat: Interface: NextUI Implemented * NextThemeProvider added * NextUIProviders added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <App />
+      </NextThemesProvider>
+    </NextUIProvider>
   </React.StrictMode>
 );
