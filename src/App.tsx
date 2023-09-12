3104dbafe3ff768c2d2fe40973a4656e901b6598
guerrerocing <guerrerocing@outlook.com>
2023-09-12 17:17:53 -0600

    Deploy: Go Live to Production

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
+          index
           path="/dashboard"
           element={
             <PrivateRoute>
